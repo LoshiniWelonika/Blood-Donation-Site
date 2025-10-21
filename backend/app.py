0ecf8c9390efc957ab017eb67b1a92075dcc3007
LoshiniWelonika <welonikakedapanawala@gmail.com>
2025-10-21 18:39:23 +0530

    Database created and user model created

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+
+#Configure SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blood_donation.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app) 
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
