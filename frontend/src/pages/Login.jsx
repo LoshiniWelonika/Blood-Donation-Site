0559b615bd2ffda88ff14b54fc31f0f3080b6b6f
LoshiniWelonika <welonikakedapanawala@gmail.com>
2025-10-24 19:16:19 +0530

    Login Part Completed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,47 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import { useState } from "react";
 import "../static/Login.css"  
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email:"",
+    password:""
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://127.0.0.1:5000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      console.log("Response status:", response.status);
+      console.log("Response data:", data);
+
+      if (response.ok) {
+        navigate(`/donordashboard/${ data.user_id}`, { state:  {userId: data.user_id} });
+      } else {
+        alert(data.error || "Login failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Something went wrong. Try again!");
+    }
+  };
+
     return(
   <div class="page">
       <div class="card">
@@ -11,17 +51,21 @@ const Login = () => {
         <form action="#" method="post">
           <div class="field">
             <label for="email">Email Address</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email" required />
+            <input type="email" id="email" name="email" placeholder="Enter your email" 
+            value={formData.email}
+            onChange={handleChange} required />
           </div>
 
           <div class="field">
             <label for="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="Enter your password" required />
+            <input type="password" id="password" name="password" placeholder="Enter your password"
+             value={formData.password}
+             onChange={handleChange} required />
           </div>
 
           <div class="actions">
-            <button type="submit" class="btn primary">Login</button>
-            <a href="register.html" class="btn ghost">Create Account</a>
+            <button type="submit" class="btn primary" onClick={handleSubmit}>Login</button>
+            <Link to={'/signup'} class="btn ghost">Create Account </Link>
           </div>
         </form>
       </div>
