2f8a2af3567a19e48311b9a1b5afffa273020a48
LoshiniWelonika <welonikakedapanawala@gmail.com>
2025-10-29 22:18:09 +0530

    Footer Added

---
+++ frontend/src/Components/home/Footer.jsx
@@ -0,0 +1,48 @@
+import { Facebook, Instagram, Twitter, Mail } from "lucide-react";
+import "../../static/home.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+
+        {/* Column 1 */}
+        <div className="footer-col">
+          <h3 className="footer-title">BloodCare</h3>
+          <p className="footer-text">
+            Saving lives, one drop at a time. Join our mission to make blood easily available for all in need.
+          </p>
+        </div>
+
+        {/* Column 2 */}
+        <div className="footer-col">
+          <h4>Quick Links</h4>
+          <ul className="footer-links">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#events">Events</a></li>
+            <li><a href="#donate">Become a Donor</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+
+        {/* Column 3 */}
+        <div className="footer-col">
+          <h4>Follow Us</h4>
+          <div className="social-icons">
+            <a href="#"><Facebook size={20} /></a>
+            <a href="#"><Instagram size={20} /></a>
+            <a href="#"><Twitter size={20} /></a>
+            <a href="#"><Mail size={20} /></a>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="footer-bottom">
+        <p>© {new Date().getFullYear()} BloodCare. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import NavBar from '../Components/home/NavBar';
 import TopicSection from '../Components/home/TopicSection'; 
 import SecondSection from '../Components/home/SecondSection';
 import ThirdSection from '../Components/home/ThirdSection';
+import Footer from '../Components/home/Footer';
 import "../static/home.css"
 
 function Home() { 
@@ -11,6 +12,7 @@ function Home() {
      <TopicSection />
      <SecondSection />
      <ThirdSection />
+     <Footer />
     </div>
   );
 }
